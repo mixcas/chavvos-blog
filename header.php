810d06821cb435f1c4e7b9fa1cfa2fb842c55a21
cas <casska@gmail.com>
2015-04-18 15:01:22 -0500

    add - new fonts

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
   <link rel="apple-touch-icon" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon-touch.png">
   <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon.png">
 
+  <link href='http://fonts.googleapis.com/css?family=Rock+Salt|Oswald:400,300,700' rel='stylesheet' type='text/css'>
 
   <script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/modernizr.js"></script>
   <script type="text/javascript">
